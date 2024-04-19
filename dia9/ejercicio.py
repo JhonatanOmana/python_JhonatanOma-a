b22366e6b66f152ff45e30d5ab0c2eebd1798cdf
JhonatanOmana <160262647+JhonatanOmana@users.noreply.github.com>
2024-04-19 12:55:15 -0500

    Add files via upload

---
+++ dia9/ejercicio.py
@@ -0,0 +1,14 @@
+carrito = []
+   
+print("=========================================================================")
+producto = str(input("Ingresa el nombre del articulo: "))
+precio = int(input("ingresa el precio del articulo : "))
+cantidad = int(input("Ingresa la cantidad: ")) 
+
+carrito.append((producto,precio,cantidad))
+print("==========================================================================")
+print("producto agregado al carrito con exito")   
+print(carrito)
+
+
+
